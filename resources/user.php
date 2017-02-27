0b49d0aba73eb8a9df9e6dda2088a5941006eba4
Cory Fail <me@coryfail.com>
2017-02-27 00:15:57 -0500

    Update user.php

--- resources/user.php
+++ resources/user.php
@@ -7,7 +7,7 @@ class User extends Controller
     public function get($id='')
     {   
         // Call any model
-        $users = $this->model->getUsers();
+        $users = $this->user->getUsers();
         
         // Include a response code
         http_response_code(200);
