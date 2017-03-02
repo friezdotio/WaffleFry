e940130f15cd67c11d350571af3525fecd8af51f
“Cory <me@coryfail.com>
2017-03-01 23:40:01 -0500

    Add example on how to pass in ajax data values and still have the dashboard work

--- resources/user.php
+++ resources/user.php
@@ -14,4 +14,15 @@ class User extends Controller
         
         print json_encode($users);
     }
+    
+    public function update($id=null)
+    {   
+        $id = $_POST["id"];
+        
+        // Include a response code
+        http_response_code(200);
+        
+        print json_encode($_POST["id"]);
+    }
+    
 }
