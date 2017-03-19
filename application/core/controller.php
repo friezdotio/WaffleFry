5621a18740bf3ed6625bfaefb216c56dcf7dc73a
“Cory <me@coryfail.com>
2017-03-19 13:30:05 -0400

    Fixed Origin issue with Cors. Some browsers did not use the Origin headers

--- application/core/controller.php
+++ application/core/controller.php
@@ -66,12 +66,10 @@ class Controller
     public function cors() {
 
         // Allow from any origin
-        if (isset($_SERVER['HTTP_ORIGIN'])) {
-            // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
-            // you want to allow, and if so:
-            header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        if (isset($_SERVER['HTTP_REFERER'])) {
+            header("Access-Control-Allow-Origin: {$_SERVER['HTTP_REFERER']}");
             header('Access-Control-Allow-Credentials: true');
-            header('Access-Control-Max-Age: 86400');    // cache for 1 day
+            header('Access-Control-Max-Age: 86400');
         }
 
         // Access-Control headers are received during OPTIONS requests
@@ -79,7 +77,7 @@ class Controller
 
             if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
                 // may also be using PUT, PATCH, HEAD etc
-                header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+                header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');       
 
             if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
                 header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
